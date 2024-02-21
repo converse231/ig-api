104417b98f9679d2c37ddb77a95d94fe8283fd7a
converse231 <paragasdaniel23@gmail.com>
2024-02-21 15:15:24 +0800

    fix fix fix

--- app/page.js
+++ app/page.js
@@ -1,11 +1,14 @@
+import Feed from "@/components/Feed/Feed";
+import { fetchPosts } from "@/lib/data";
 import Link from "next/link";
 
-export default function Home() {
+export default async function Home({ searchParams }) {
+  const index = searchParams.index || "";
+  const data = await fetchPosts();
   return (
-    <div className="flex justify-center h-screen items-center w-screen gap-3">
-      <Link className="text-blue-400 underline uppercase" href={"/layouts"}>
-        Go to layouts
-      </Link>
+    <div className="flex flex-col items-center w-screen gap-3">
+      <h1 className="text-4xl font-bol mt-10">IG Layouts</h1>
+      {data && <Feed data={data} index={index} />}
     </div>
   );
 }
